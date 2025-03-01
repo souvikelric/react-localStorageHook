290c45ebd6a27bc939dbf235b1a04c161b0d730e
Souvik <souvikelric@gmail.com>
2025-03-01 22:46:41 +0530

    some minor changes to Counter component

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,14 +1,9 @@
-import { useEffect } from "react";
 import useLocalStorage from "../../hooks/useLocalStorage";
 import styles from "./Counter.module.css";
 export default function Counter() {
-  const [value, changeValue] = useLocalStorage<number>({
+  const { value, setValue: changeValue } = useLocalStorage<number>({
     key: "counter",
     initialValue: 0,
-  });
-  useEffect(() => {
-    console.log(value);
-    // console.log(setValue);
   });
   return (
     <div className={styles.counter}>
